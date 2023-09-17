0d424337a4765bd11c86e0c9388ff6b93d27064f
Ruba <91896958+RubaUser@users.noreply.github.com>
2023-09-17 11:51:58 +0300

    first commit

---
+++ lib/Screens/welcomeScreen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class welcomeScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Stack(children: [
+          Container(
+               width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height / 1.6,
+        decoration: BoxDecoration(color: Colors.white),
+          ),
+                    Container(
+               width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height / 1.6,
+        decoration: BoxDecoration(color: Color(0xFF674AFF)),
+          ),
+        ],),
+      ),
+    );
+  }
+
+}
