f46d0bb8b941899358aa271262f413a934b3155b
Ruba <91896958+RubaUser@users.noreply.github.com>
2023-09-22 06:35:04 +0300

    style homeScreen

--- lib/Screens/homeScreen.dart
+++ lib/Screens/homeScreen.dart
@@ -1,9 +1,128 @@
 import 'package:flutter/material.dart';
 
-class homScreen extends StatelessWidget{
+class homScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-  return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(
+            height: 40,
+          ),
+          // Hero Section
+
+          Container(
+            padding: EdgeInsets.only(top: 15, left: 15, right: 15, bottom: 10),
+            decoration: BoxDecoration(
+                color: Color.fromARGB(255, 126, 54, 142),
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20))),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(
+                      Icons.dashboard,
+                      size: 30,
+                      color: Colors.white,
+                    ),
+                    Icon(
+                      Icons.notifications,
+                      size: 30,
+                      color: Colors.white,
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 3, bottom: 15),
+                  child: Text(
+                    "Hi , Ahmed",
+                    style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 1,
+                        wordSpacing: 2,
+                        color: Colors.white),
+                  ),
+                 
+                ),
+                 Container(
+                  margin: EdgeInsets.only(top:3,bottom: 20),
+                  width: MediaQuery.of(context).size.width,
+                  height: 55,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    
+                  ),
+                 )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 250),
+            child: Text(
+              "All Courses",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                letterSpacing: 2,
+                wordSpacing: 2,
+              ),
+            ),
+          ),
+
+          // GridView
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16.0,
+                  mainAxisSpacing: 16.0,
+                ),
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  return Card(
+                    elevation: 4.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            "images/Python.png",
+                            height: 70,
+                            width: 70,
+                          ),
+                          SizedBox(height: 8.0),
+                          Text(
+                            'Item $index',
+                            style: TextStyle(fontSize: 16.0),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
-  
 }
