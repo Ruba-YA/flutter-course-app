4378f835443ac0ae3ae84113aa304ac02df89031
Ruba <91896958+RubaUser@users.noreply.github.com>
2023-09-28 07:26:07 +0300

    replace stack widget with expanded

--- lib/Screens/welcomeScreen.dart
+++ lib/Screens/welcomeScreen.dart
@@ -1,70 +1,75 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_8/Screens/homeScreen.dart';
-
+import 'package:flutter_application_8/const.dart';
 class welcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Stack(
+     body: SafeArea(
+       child: Column(
           children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height / 1.6,
-              decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 207, 223, 159),
-                  borderRadius:
-                      BorderRadius.only(bottomRight: Radius.circular(70))),
-              child: Image.asset(
-                "images/home.gif",
-                alignment: Alignment.centerRight,
-                scale: 0.8,
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomLeft,
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height / 2.666,
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 207, 223, 159),
-                ),
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                padding: EdgeInsets.only(top: 40, bottom: 30),
-                height: MediaQuery.of(context).size.height / 2.666,
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 255, 255, 255),
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(70)),
-                ),
-                child: Column(
-                  children: [
-                    Text(
-                      "keep learning ",
-                      style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.w600,
-                          letterSpacing: 1,
-                          wordSpacing: 2),
+              Expanded(
+                  flex: 4,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    decoration: const BoxDecoration(
+                        color: secondaryColor,
+                        borderRadius:
+                            BorderRadius.only(bottomRight: Radius.circular(70))),
+                    child: Column(
+                      children: [
+                        Expanded(child: Image.asset("images/home.gif",scale: 0.8,))
+                      ],
                     ),
-                    SizedBox(height: 15),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 40),
-                      child: Text(
-                        "keep learning Learning with us..",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 17, color: Colors.black.withOpacity(0.6)),
-                      ),
-                    ),
-                    SizedBox(height: 60),
-                   Material(
+                  )),
+              Expanded(
+                  flex: 2,
+                  child: Container(
+
+                    color: secondaryColor,
+                    child: Container(
+                      width: double.infinity,
+                      decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(70),
+                          )),
+                      child: Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                         
+                            Text(
+                              "Keep Learning ",
+                              style: TextStyle(
+                                fontSize: 28,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 2,
+                                wordSpacing: 3
+                              ),
+                            ),
+                      SizedBox(height: 7,),
+                            Text(
+                              "Keep Learning  with\nus,where you are!",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.black54,
+                                wordSpacing: 2.5,
+                                height: 1.5,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                                  letterSpacing: 2,
+                              ),
+                            ),
+                                           SizedBox(height: 30,),
+
+                            //repleace sizebox with spacer
+                            Row(
+                              //button position
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                  Material(
                     color:   Color.fromARGB(255, 126, 54, 142),
                     borderRadius: BorderRadius.circular(10),
                      child: InkWell(
@@ -82,13 +87,14 @@ class welcomeScreen extends StatelessWidget {
                        ),
                      ),
                    )
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ))
+            ],
+     )));
   }
 }
